82d1ade451a26dec00cd758b06dea9f698a669fc
Janderson <jandersonsouza.ti@gmail.com>
2022-05-26 01:09:34 -0300

    fix url image

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -6,8 +6,12 @@ export const Image: React.FC<ImageProps> = (props) => {
   return (
     <StyledImage
       {...props}
-      rounded
-      src={props.src.startsWith("./") ? props.src.substring(2) : props.src}
+      src={
+        props.src.startsWith("http")
+          ? props.src
+          : window.location.origin +
+            (props.src.startsWith("./") ? props.src.substring(2) : props.src)
+      }
     />
   );
 };
